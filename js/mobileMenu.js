1846464d9d6a2e22248c92f65b42ddb33cfdb01e
AbuAbdullohm <mmprogramming3@gmail.com>
2023-08-28 11:10:54 +0500

    some bags fixed

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -43,29 +43,94 @@ defLanguage.addEventListener("click", () => {
     otherLanguage.classList.remove("active");
 
     otherLanguage.style.display = "none";
-    console.log("first");
   } else {
     otherLanguage.classList.add("active");
     otherLanguage.style.display = "block";
-    console.log("second");
   }
 });
 
-window.onclick = function (e) {
+document.onclick = function (e) {
   if (e.target != defLanguage && e.target != otherLanguage) {
     otherLanguage.classList.remove("active");
     otherLanguage.style.display = "none";
-    console.log("third");
   }
 };
 
 const listItem = document.querySelectorAll(".list-item");
+const contentText = document.querySelector(".content-text");
+const contentTitle = document.querySelector(".content-title");
+
+let myText = [
+  {
+    id: "myText1",
+    title: "Наши услуги",
+    text: `Наш формат универсальная семейная клиника в которой взрослым и
+              детям оказывается различные виды медицинской помощи.Диагностика,
+              лечение и профилактика заболеваний практически по всем
+              направлениям современной медицины.`,
+  },
+  {
+    id: "myText2",
+    title: "Наши специалисты",
+    text: `Наших центрах работают опытные специалисты, который с вниманием
+          относится к каждому пациенту. Это профессионалы которые станут вашими
+          партнёрами в сохранении здоровья. Наша команда более 20 врачей разных
+          Специальностей Среди них 15 кандидатов медицинских наук 15 докторов
+          медицинских наук три профессора в две доцентов и ещё много очень
+          сотрудников заслуженныйе врачи Узбекистан.`,
+  },
+  {
+    id: "myText3",
+    title: "Специалисты  Viva",
+    text: `Наших медицинских центрах работают врачи как самых востребованных
+          медицинских специальностей: терапевты, кардиологии, урологии,
+          гинекологии, офтальмологи, оториноларингологии, хирурги и другие так и
+          редких: генетики, репродуктологии, эмбриологии, гепатологии и другие.
+          В детских отделениях ведут прием доктора специализирующихся на лечение
+          детей с первых дней жизни до совершеннолетия`,
+  },
+  {
+    id: "myText4",
+    title: "Хирургия",
+    text: `Для успешного лечения необходимо прогресс инновации. Десятка Врачей и
+          специалистов нашей клинике и партнёрских клиник расследует общая цель:
+          разработать новые формы терапии и как можно быстрее внедрить их на
+          практике чтобы помочь пациентам.`,
+  },
+  {
+    id: "myText5",
+    title: "Здоровья",
+    text: `Здоровья - самый ценный ресурс. Вернуть его сохранить, приумножить
+          такая задачи посилам каждому в партнёрстве c “Viva Medical Center”.
+          Осознавая Свою ответственность перед обществом “Viva Medical Center”
+          действует в облога своих пациентов и сотрудников, сохраняя их здоровье
+          и атмосферу всеобщего доверия.`,
+  },
+  {
+    id: "myText6",
+    title: "Наши услуги",
+    text: `Весь персонал, с которым я контактировал, делал все возможное, чтобы
+        помочь мне с моими медицинскими проблемами. Сейчас я веду более активный
+        образ жизни и больше не чувствую дискомфорта в груди.`,
+  },
+  {
+    id: "myText7",
+    title: "Наш формат",
+    text: `Наш формат универсальная семейная клиника в которой взрослым и
+              детям оказывается различные виды медицинской помощи.Диагностика,
+              лечение и профилактика заболеваний практически по всем
+              направлениям современной медицины.`,
+  },
+];
 
 listItem.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     listItem.forEach((item) => {
       if (e.target.innerHTML === item.innerHTML) {
         e.target.classList.add("active");
+        const randomNum = Math.floor(Math.random() * 7 + 1);
+        contentTitle.innerText = myText[randomNum].title;
+        contentText.innerHTML = myText[randomNum].text;
       } else {
         item.classList.remove("active");
       }
